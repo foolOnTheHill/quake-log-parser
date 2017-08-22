6a772c264fd90140f1f4845f22764adc15601e19
George Oliveira <ghao@cin.ufpe.br>
2017-08-22 19:25:30 -0300

    More constants

--- src/modules/Consts.js
+++ src/modules/Consts.js
@@ -1,7 +1,13 @@
 
 const Consts = {
   SUMMARY: 'summary',
-  RANK: 'rank'
+  RANK: 'rank',
+  KILL: 'Kill:',
+  WORLD: '<world>',
+  GAME_START: '------------------------------------------------------------',
+  GAME_END: '------------------------------------------------------------',
+  KILLED: 'killed',
+  BY: 'by'
 };
 
 export default Consts;
