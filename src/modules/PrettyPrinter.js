b437fbfb565844baaec691eb11e4815878aad2db
George Oliveira <ghao@cin.ufpe.br>
2017-08-22 19:29:31 -0300

    Adds PrettyPrinter

---
+++ src/modules/PrettyPrinter.js
@@ -0,0 +1,24 @@
+
+class PrettyPrinter {
+
+  printSummary(summary) {
+    for (let i = 1; i < summary.matches; i++) {
+      if (summary[i].total_kills > 0) {
+        const game_id = 'game_'+i;
+        let output = {};
+        output[game_id] = summary[i];
+        console.log(output);
+      }
+    }
+  }
+
+  printRank(rank) {
+    for (let i = 0; i < rank.players.length; i++) {
+      const playerName = rank.players[i];
+      const rankLine = [playerName, ' (', rank.kills[playerName], ')'];
+      console.log(rankLine.join(''));
+    }
+  }
+}
+
+export default PrettyPrinter;
