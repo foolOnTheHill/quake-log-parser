b7a542b72bc3d95780060733e0aaa771fd2f0799
George Oliveira <ghao@cin.ufpe.br>
2017-08-24 11:30:12 -0300

    Updates Consts.js to include html command token

--- src/modules/Consts.js
+++ src/modules/Consts.js
@@ -3,6 +3,7 @@
 const Consts = {
   SUMMARY: 'summary',
   RANK: 'rank',
+  HTML: 'html',
   KILL: 'Kill:',
   WORLD: '<world>',
   GAME_START: '------------------------------------------------------------',
